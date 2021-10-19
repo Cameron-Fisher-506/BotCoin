0b241ec8d89490230fae4a0478587cdb99523f45
Cameron-Fisher-506 <fishercameron506@icloud.com>
2021-10-19 08:48:18 +0200

    Fibonacci Retracement Update 04
    - StraightLineFormulaUtilsTest.kt: Updated method names and added more tests.

--- app/src/androidTest/java/za/co/botcoin/StraightLineFormulaUtilsTest.kt
+++ app/src/androidTest/java/za/co/botcoin/StraightLineFormulaUtilsTest.kt
@@ -3,7 +3,10 @@ package za.co.botcoin
 import junit.framework.Assert.assertEquals
 import org.junit.Before
 import org.junit.Test
+import za.co.botcoin.utils.StraightLineFormulaUtils.calculateConstant
 import za.co.botcoin.utils.StraightLineFormulaUtils.calculateGradient
+import za.co.botcoin.utils.StraightLineFormulaUtils.calculateX
+import za.co.botcoin.utils.StraightLineFormulaUtils.calculateY
 
 class StraightLineFormulaUtilsTest {
 
@@ -13,7 +16,7 @@ class StraightLineFormulaUtilsTest {
     }
 
     @Test
-    fun should_calculate_gradientSuccess() {
+    fun shouldCalculateGradientSuccess() {
         //given
         val pointA = Pair(3.0, 2.0)
         val pointB = Pair(1.0, 1.0)
@@ -27,7 +30,7 @@ class StraightLineFormulaUtilsTest {
     }
 
     @Test
-    fun should_calculate_gradientZero() {
+    fun shouldCalculateGradientZeroSuccess() {
         //given
         val pointA = Pair(3.0, 2.0)
         val pointB = Pair(1.0, 2.0)
@@ -41,7 +44,7 @@ class StraightLineFormulaUtilsTest {
     }
 
     @Test
-    fun should_calculate_gradientFailure() {
+    fun shouldCalculateGradientFailure() {
         //given
         val pointA = Pair(3.0, 2.0)
         val pointB = Pair(3.0, 1.0)
@@ -49,6 +52,50 @@ class StraightLineFormulaUtilsTest {
 
         //when
         val actual = calculateGradient(pointA.second, pointB.second, pointA.first, pointB.first)
+
+        //then
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun shouldCalculateConstantSuccess() {
+        //given
+        val point = Pair(3.0, 2.0)
+        val m = 0.5
+        val expected = 0.5
+
+        //when
+        val actual = calculateConstant(point.first, point.second, m)
+
+        //then
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun shouldCalculateXSuccess() {
+        //given
+        val y = 2.0
+        val m = 0.5
+        val c = 0.5
+        val expected = 3.0
+
+        //when
+        val actual = calculateX(y, m, c)
+
+        //then
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun shouldCalculateYSuccess() {
+        //given
+        val x = 3.0
+        val m = 0.5
+        val c = 0.5
+        val expected = 2.0
+
+        //when
+        val actual = calculateY(x, m, c)
 
         //then
         assertEquals(expected, actual)
