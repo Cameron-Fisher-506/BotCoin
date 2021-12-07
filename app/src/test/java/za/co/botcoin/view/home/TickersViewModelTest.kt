8fdc87bb522349be0b6b80f270385be1332ad769
Cameron-Fisher-506 <fishercameron506@icloud.com>
2021-12-07 13:48:34 +0200

    Fibonacci Retracement Update 12
    - build.gradle: Added dependencies.
    - gradle.properties: Added property to resolve Unit Test error.
    - TickersViewModelTest.kt: Using RobolectricTestRunner.
    - TickersViewModelStateVerificationTest.kt: imported Assert class.

--- app/src/test/java/za/co/botcoin/view/home/TickersViewModelTest.kt
+++ app/src/test/java/za/co/botcoin/view/home/TickersViewModelTest.kt
@@ -1,25 +1,22 @@
 package za.co.botcoin.view.home
 
-import android.app.Application
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import org.junit.Before
 import org.junit.Rule
 import org.junit.runner.RunWith
 import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
 import org.mockito.junit.MockitoJUnit
-import org.mockito.junit.MockitoJUnitRunner
 import org.mockito.junit.MockitoRule
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
+import org.robolectric.annotation.Config
 import za.co.botcoin.model.repository.tickers.TickersRepository
 import za.co.botcoin.model.repository.tickers.TickersViewModel
 
-@RunWith(MockitoJUnitRunner::class)
-abstract class TickersViewModelTest {   
-
-    protected val tickersRepository: TickersRepository = Mockito.mock(TickersRepository::class.java)
-
-    private val application = Mockito.mock(Application::class.java)
-
-    protected lateinit var tickersViewModel: TickersViewModel
+@RunWith(RobolectricTestRunner::class)
+@Config(manifest=Config.NONE)
+abstract class TickersViewModelTest {
 
     @Rule
     @JvmField
@@ -29,8 +26,14 @@ abstract class TickersViewModelTest {
     @JvmField
     var initRule: MockitoRule = MockitoJUnit.rule()
 
+    protected val tickersRepository: TickersRepository = Mockito.mock(TickersRepository::class.java)
+
+    protected lateinit var tickersViewModel: TickersViewModel
+
     @Before
     fun setUp() {
+        MockitoAnnotations.openMocks(this);
+        val application = RuntimeEnvironment.getApplication()
         tickersViewModel = TickersViewModel(application)
         tickersViewModel.repository = tickersRepository
     }
