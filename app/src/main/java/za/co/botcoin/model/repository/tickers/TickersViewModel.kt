6ecde5a28b3b8f835b8b5702568ae95625592fed
Cameron-Fisher-506 <fishercameron506@gmail.com>
2022-07-02 07:05:39 +0200

    Cleanup Update 03: Added Dependency Injection Using Dagger 2

--- app/src/main/java/za/co/botcoin/model/repository/tickers/TickersViewModel.kt
+++ app/src/main/java/za/co/botcoin/model/repository/tickers/TickersViewModel.kt
@@ -2,19 +2,20 @@ package za.co.botcoin.model.repository.tickers
 
 import android.app.Application
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import za.co.botcoin.model.models.Ticker
 import za.co.botcoin.model.repository.BaseViewModel
 import za.co.botcoin.utils.Resource
 
-class TickersViewModel(application: Application) : BaseViewModel(application) {
-    var repository: TickersRepository = TickersRepository(application)
+class TickersViewModel(var repository: TickersRepository) : ViewModel() {
 
     lateinit var tickersLiveData: LiveData<Resource<List<Ticker>>>
 
     fun fetchTickers() {
-        tickersLiveData = liveData(ioDispatcher) {
+        tickersLiveData = liveData(Dispatchers.IO) {
             emit(repository.fetchTickers())
         }
     }
