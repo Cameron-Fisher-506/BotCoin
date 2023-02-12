a4cfd5eb3b254f8a6f66e1337169d3fe4680b1a8
Cameron-Fisher-506 <fishercameron506@gmail.com>
2023-02-12 09:30:50 +0200

    Cleanup 03: Added Corelib for custom views

---
+++ corelib/src/main/java/com/example/corelib/informational/InformationView.kt
@@ -0,0 +1,30 @@
+package com.example.corelib.informational
+
+import android.content.Context
+import android.content.res.TypedArray
+import android.util.AttributeSet
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.corelib.R
+import com.example.corelib.databinding.InformationViewBinding
+
+class InformationView(context: Context, attrs: AttributeSet?, defStyleAttr: Int = 0): ConstraintLayout(context, attrs, defStyleAttr) {
+    private lateinit var binding: InformationViewBinding
+
+    init {
+        inflate(context, R.layout.information_view, this)
+        binding = InformationViewBinding.bind(this)
+
+        val typedArray: TypedArray = context.obtainStyledAttributes(attrs, R.styleable.InformationView, defStyleAttr, 0)
+
+        typedArray.recycle()
+    }
+
+    fun setInformationTextView(text: String) {
+        binding.informationTextView.text = text
+    }
+
+    fun setOnInformationImageButtonClickListener(onClick: () -> Unit) {
+        onClick.invoke()
+    }
+}
