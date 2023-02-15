e7bd5e845d5eea782939a4a3d8df230c0140eaa5
Cameron-Fisher-506 <fishercameron506@gmail.com>
2023-02-15 07:05:58 +0200

    Cleanup 04: Update Menu Screens To Use InformationView UI Component

--- app/src/main/java/za/co/botcoin/view/menu/donate/DonateMenuDonateFragment.kt
+++ app/src/main/java/za/co/botcoin/view/menu/donate/DonateMenuDonateFragment.kt
@@ -12,7 +12,7 @@ import za.co.botcoin.view.menu.MenuBaseFragment
 
 class DonateMenuDonateFragment : MenuBaseFragment(R.layout.donate_fragment) {
     private lateinit var binding: DonateFragmentBinding
-    private val donateViewModel by viewModels<DonateMenuDonateViewModel>(factoryProducer = { menuActivity.getViewModelFactory })
+    private val donateMenuDonateViewModel by viewModels<DonateMenuDonateViewModel>(factoryProducer = { menuActivity.getViewModelFactory })
 
     private var asset: String = ""
 
@@ -27,8 +27,8 @@ class DonateMenuDonateFragment : MenuBaseFragment(R.layout.donate_fragment) {
     }
 
     private fun receiveAndObserveReceive() {
-        this.donateViewModel.receive(asset, BuildConfig.KEY_ID, BuildConfig.SECRET_KEY)
-        this.donateViewModel.receiveResponse.observe(viewLifecycleOwner) {
+        this.donateMenuDonateViewModel.receive(asset, BuildConfig.KEY_ID, BuildConfig.SECRET_KEY)
+        this.donateMenuDonateViewModel.receiveResponse.observe(viewLifecycleOwner) {
             when (it.status) {
                 Status.SUCCESS -> {
                     menuActivity.dismissProgressBar()
@@ -61,8 +61,8 @@ class DonateMenuDonateFragment : MenuBaseFragment(R.layout.donate_fragment) {
     }
 
     private fun sendAndObserveSend(amount: String, asset: String, address: String, destinationTag: String) {
-        this.donateViewModel.send(amount, asset, address, destinationTag)
-        this.donateViewModel.sendResponse.observe(viewLifecycleOwner) {
+        this.donateMenuDonateViewModel.send(amount, asset, address, destinationTag)
+        this.donateMenuDonateViewModel.sendResponse.observe(viewLifecycleOwner) {
             when (it.status) {
                 Status.SUCCESS -> {
                     menuActivity.dismissProgressBar()
@@ -71,20 +71,20 @@ class DonateMenuDonateFragment : MenuBaseFragment(R.layout.donate_fragment) {
                         displayDonateOptions()
                         data.map { response ->
                             if (response.success) {
-                                donateViewModel.displaySentAmountOfAssetToAddressNotification(amount, asset, address)
+                                donateMenuDonateViewModel.displaySentAmountOfAssetToAddressNotification(amount, asset, address)
                             } else {
-                                donateViewModel.displaySendFailedNotification()
+                                donateMenuDonateViewModel.displaySendFailedNotification()
                             }
                         }
                     } else {
                         displayErrorMessage()
-                        donateViewModel.displaySendFailedNotification()
+                        donateMenuDonateViewModel.displaySendFailedNotification()
                     }
                 }
                 Status.ERROR -> {
                     menuActivity.dismissProgressBar()
                     displayErrorMessage()
-                    donateViewModel.displaySendFailedNotification()
+                    donateMenuDonateViewModel.displaySendFailedNotification()
                 }
                 Status.LOADING -> {
                     menuActivity.displayProgressBar()
@@ -97,10 +97,10 @@ class DonateMenuDonateFragment : MenuBaseFragment(R.layout.donate_fragment) {
 
     private fun setUpOnClickListeners() {
         this.binding.copyImageButton.setOnClickListener {
-            donateViewModel.copyToClipBoard(this.binding.addressEditText.text.toString())
+            donateMenuDonateViewModel.copyToClipBoard(this.binding.addressEditText.text.toString())
         }
         this.binding.copyTagImageButton.setOnClickListener {
-            donateViewModel.copyToClipBoard(this.binding.tagEditText.text.toString())
+            donateMenuDonateViewModel.copyToClipBoard(this.binding.tagEditText.text.toString())
         }
         this.binding.donateButton.setOnClickListener {
             if (GeneralUtils.isApiKeySet(context)) {
@@ -110,11 +110,14 @@ class DonateMenuDonateFragment : MenuBaseFragment(R.layout.donate_fragment) {
                 if (amount.isNotBlank() && amount != "0") {
                     sendAndObserveSend(amount, asset, address, destinationTag)
                 } else {
-                    donateViewModel.displayInvalidAmountEnteredAlertDialog(asset)
+                    donateMenuDonateViewModel.displayInvalidAmountEnteredAlertDialog(asset)
                 }
             } else {
-                donateViewModel.displayLunoApiCredentialsAlertDialog()
+                donateMenuDonateViewModel.displayLunoApiCredentialsAlertDialog()
             }
+        }
+        binding.donateInformationView.setOnClickListener {
+            donateMenuDonateViewModel.displayDonateDescriptionAlertDialog()
         }
     }
 
