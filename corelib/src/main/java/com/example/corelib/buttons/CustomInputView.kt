1264f61e02d7681d69ee9a7b4a0bf0a9a0ae2c95
Cameron-Fisher-506 <fishercameron506@gmail.com>
2023-02-18 09:00:44 +0200

    Cleanup 07: Create Edit Text Custom View

---
+++ corelib/src/main/java/com/example/corelib/buttons/CustomInputView.kt
@@ -0,0 +1,27 @@
+package com.example.corelib.buttons
+
+import android.content.Context
+import android.content.res.TypedArray
+import android.util.AttributeSet
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.corelib.R
+import com.example.corelib.databinding.CustomInputViewBinding
+
+class CustomInputView @JvmOverloads constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int = 0): ConstraintLayout(context, attrs, defStyleAttr) {
+    private lateinit var binding: CustomInputViewBinding
+
+    init {
+        inflate(context, R.layout.custom_input_view, this)
+        binding = CustomInputViewBinding.bind(this)
+
+        val typedArray: TypedArray = context.obtainStyledAttributes(attrs, R.styleable.CustomInputView, defStyleAttr, 0)
+        binding.descriptionTextView.text = typedArray.getText(R.styleable.CustomInputView_description)
+        typedArray.recycle()
+    }
+
+    fun setDescription(description: String) {
+        binding.descriptionTextView.text = description
+        binding.descriptionTextView.visibility = View.VISIBLE
+    }
+}
