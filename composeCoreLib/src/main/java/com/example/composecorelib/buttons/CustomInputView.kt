322f3ea866389b719832af0cbf418dccc8b6ece3
Cameron-Fisher-506 <fishercameron506@gmail.com>
2023-12-11 19:35:29 +0200

    Jetpack Compose 01: Added compose component

--- composeCoreLib/src/main/java/com/example/composecorelib/buttons/CustomInputView.kt
+++ composeCoreLib/src/main/java/com/example/composecorelib/buttons/CustomInputView.kt
@@ -1,31 +1,27 @@
-package com.example.corelib.buttons
+package com.example.composecorelib.buttons
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
-
 @Composable
 fun CustomInputView(
     title: String = "Title",
     value: String = "Value",
-    description: String = "Description",
-    errorMessage: String = "Error"
+    description: String = "description",
+    errorMessage: String = "error"
 ) {
     Surface(modifier = Modifier.wrapContentSize()) {
         Column(modifier = Modifier.fillMaxWidth(1f)) {
@@ -34,25 +30,45 @@ fun CustomInputView(
                 modifier = Modifier.padding(horizontal = 20.dp, vertical = 10.dp),
                 style = MaterialTheme.typography.titleSmall
             )
-            TextField(value = value, modifier = Modifier
-                .padding(horizontal = 20.dp, vertical = 10.dp)
-                .fillMaxWidth(1f), onValueChange = {})
+            if (errorMessage.isNotEmpty()) {
+                TextFieldView(value)
+            } else {
+                TextFieldView(value, true)
+            }
+
             Row(
                 modifier = Modifier.fillMaxWidth(1f),
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
-                Text(
-                    text = description,
-                    modifier = Modifier.padding(horizontal = 20.dp, vertical = 10.dp)
-                )
-                Text(
-                    text = errorMessage,
-                    modifier = Modifier.padding(horizontal = 20.dp, vertical = 10.dp),
-                    color = Color.Red
-                )
+                if (description.isNotEmpty()) {
+                    Text(
+                        text = description,
+                        modifier = Modifier.padding(horizontal = 20.dp, vertical = 10.dp)
+                    )
+                }
+
+                if (errorMessage.isNotEmpty()) {
+                    Text(
+                        text = errorMessage,
+                        modifier = Modifier.padding(horizontal = 20.dp, vertical = 10.dp),
+                        color = Color.Red
+                    )
+                }
             }
         }
     }
+}
+
+@Composable
+fun TextFieldView(value: String, isError: Boolean = false) {
+    TextField(
+        value = value,
+        isError = isError,
+        modifier = Modifier
+            .padding(horizontal = 20.dp, vertical = 10.dp)
+            .fillMaxWidth(1f),
+        onValueChange = {}
+    )
 }
 
 @Preview(showBackground = true)
