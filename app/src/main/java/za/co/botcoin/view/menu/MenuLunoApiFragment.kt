20a9ade014929731786391496d72500d70aa8c01
Cameron-Fisher-506 <fishercameron506@gmail.com>
2023-12-20 18:06:11 +0200

    Jetpack Compose 02: Updated Luno API Screen To Compose

--- app/src/main/java/za/co/botcoin/view/menu/MenuLunoApiFragment.kt
+++ app/src/main/java/za/co/botcoin/view/menu/MenuLunoApiFragment.kt
@@ -1,12 +1,12 @@
 package za.co.botcoin.view.menu
 
 import android.os.Bundle
-import android.provider.Settings.Global.getString
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -15,48 +15,65 @@ import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.fragment.app.viewModels
+import com.example.composecorelib.buttons.ButtonView
 import com.example.composecorelib.buttons.CustomInputViewCompose
 import com.example.composecorelib.informational.InformationViewCompose
-import za.co.botcoin.R
+import za.co.botcoin.utils.ConstantUtils
 
-class MenuLunoApiFragment : MenuBaseFragment(R.layout.luno_api_fragment) {
+class MenuLunoApiFragment : MenuBaseFragment() {
     private val menuLunoApiViewModel by viewModels<MenuLunoApiViewModel>(factoryProducer = { menuActivity.getViewModelFactory })
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        //binding = LunoApiFragmentBinding.bind(view)
-
-        setUpViews()
-        setUpOnClickListeners()
-    }
-
-    private fun setUpViews() {
-       /* binding.keyIdCustomInputView.setText(ConstantUtils.USER_KEY_ID)
-        binding.secretKeyCustomInputView.setText(ConstantUtils.USER_SECRET_KEY)*/
-    }
-
-    private fun setUpOnClickListeners() {
-        /*this.binding.saveButton.setOnClickListener {
-            val keyId = this.binding.keyIdCustomInputView.getText()
-            val secretKey = this.binding.secretKeyCustomInputView.getText()
-
-            if (keyId.isNotBlank() && secretKey.isNotBlank()) {
-                ConstantUtils.USER_KEY_ID = keyId
-                ConstantUtils.USER_SECRET_KEY = secretKey
-
-                menuLunoApiViewModel.saveLunoApiKeyId(keyId)
-                menuLunoApiViewModel.saveLunoApiSecretKey(secretKey)
-                menuLunoApiViewModel.displayApiKeySavedToast()
-            } else {
-                menuLunoApiViewModel.displayLunoApiCredentialsAlertDialog()
+    @OptIn(ExperimentalMaterial3Api::class)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                Scaffold(topBar = {
+                    TopAppBar(
+                        title = { Text(text = "Luno API") },
+                        colors = topAppBarColors(
+                            containerColor = Color(0xFF1976D2),
+                            titleContentColor = Color(0xFFFFFFFF),
+                        )
+                    )
+                }) {
+                    Surface(Modifier.padding(it)) {
+                        Column {
+                            InformationViewCompose("Information") {
+                                menuLunoApiViewModel.displayLunoApiCredentialsInformationAlertDialog()
+                            }
+                            CustomInputViewCompose("Key ID", ConstantUtils.USER_KEY_ID)
+                            CustomInputViewCompose("Secret Key", ConstantUtils.USER_SECRET_KEY)
+                            ButtonView("Save") {
+                                saveData()
+                            }
+                        }
+                    }
+                }
             }
         }
+    }
 
-       binding.lunoApiCredentialsInformationView.setOnClickListener {
-            menuLunoApiViewModel.displayLunoApiCredentialsInformationAlertDialog()
-        }*/
+    private fun saveData() {
+        val keyId = ""
+        val secretKey = ""
+
+        if (keyId.isNotBlank() && secretKey.isNotBlank()) {
+            ConstantUtils.USER_KEY_ID = keyId
+            ConstantUtils.USER_SECRET_KEY = secretKey
+
+            menuLunoApiViewModel.saveLunoApiKeyId(keyId)
+            menuLunoApiViewModel.saveLunoApiSecretKey(secretKey)
+            menuLunoApiViewModel.displayApiKeySavedToast()
+        } else {
+            menuLunoApiViewModel.displayLunoApiCredentialsAlertDialog()
+        }
     }
 }
 
@@ -65,17 +82,24 @@ class MenuLunoApiFragment : MenuBaseFragment(R.layout.luno_api_fragment) {
 @Composable
 fun MenuLunoApiScreen() {
     Scaffold(topBar = {
-        TopAppBar(title = { Text(text = "Luno API") },
+        TopAppBar(
+            title = { Text(text = "Luno API") },
             colors = topAppBarColors(
                 containerColor = Color(0xFF1976D2),
                 titleContentColor = Color(0xFFFFFFFF),
-            ))
+            )
+        )
     }) {
         Surface(Modifier.padding(it)) {
             Column {
-                InformationViewCompose("Information")
-                CustomInputViewCompose("Key ID")
-                CustomInputViewCompose("Secret Key")
+                InformationViewCompose("Information") {
+
+                }
+                CustomInputViewCompose("Key ID", ConstantUtils.USER_KEY_ID)
+                CustomInputViewCompose("Secret Key", ConstantUtils.USER_SECRET_KEY)
+                ButtonView("Save") {
+                    
+                }
             }
         }
     }
--- composeCoreLib/src/main/java/com/example/composecorelib/informational/InformationViewCompose.kt
+++ composeCoreLib/src/main/java/com/example/composecorelib/informational/InformationViewCompose.kt
@@ -2,11 +2,15 @@ package com.example.composecorelib.informational
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -21,24 +25,32 @@ import androidx.compose.ui.unit.dp
 import com.example.composecorelib.R
 
 @Composable
-fun InformationViewCompose(title: String) {
+fun InformationViewCompose(title: String, clickable: () -> Unit) {
     Surface(Modifier.wrapContentSize()) {
-        Row(
-            Modifier
-                .fillMaxWidth(1f),
-            Arrangement.SpaceBetween,
-            Alignment.CenterVertically
-        ) {
-            Text(
-                title,
-                Modifier.padding(20.dp),
-                style = MaterialTheme.typography.titleMedium
-            )
-            Image(
-                painterResource(R.drawable.ic_info_black_24dp),
-                stringResource(R.string.information),
-                Modifier.padding(20.dp)
-            )
+        Column {
+            Row(
+                Modifier
+                    .fillMaxWidth(1f)
+                    .clickable { clickable() },
+                Arrangement.SpaceBetween,
+                Alignment.CenterVertically
+            ) {
+                Text(
+                    title,
+                    Modifier.padding(20.dp),
+                    style = MaterialTheme.typography.titleMedium
+                )
+                Image(
+                    painterResource(R.drawable.ic_info_black_24dp),
+                    stringResource(R.string.information),
+                    Modifier.padding(20.dp)
+                )
+            }
+            Divider(Modifier
+                .fillMaxWidth()
+                .width(1.dp)
+                .padding(start = 20.dp, end = 20.dp, bottom = 5.dp),
+                color = Color.LightGray)
         }
     }
 }
@@ -46,5 +58,5 @@ fun InformationViewCompose(title: String) {
 @Preview(showBackground = true)
 @Composable
 fun Preview() {
-    InformationViewCompose("Information")
+    InformationViewCompose("Information") {}
 }
