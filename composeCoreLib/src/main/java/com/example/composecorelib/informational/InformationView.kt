f2a395d44992b203acaa62cdcab0589a72334484
Cameron-Fisher-506 <fishercameron506@gmail.com>
2024-04-07 15:10:10 +0200

    Jetpack Compose 04: Updated Compose Custom Views

--- composeCoreLib/src/main/java/com/example/composecorelib/informational/InformationView.kt
+++ composeCoreLib/src/main/java/com/example/composecorelib/informational/InformationView.kt
@@ -1,23 +1,19 @@
 package com.example.composecorelib.informational
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
